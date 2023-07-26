55dbb6d4976c71fdbf83f72c30e75780d8c5a618
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-07-26 17:10:57 +0300

    Complited

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        boolean isLeap = false;
+
+        if (year % 4 == 0) {
+            if (year % 100 != 0 || year % 400 == 0) {
+                isLeap = true;
+            }
+        }
+
+        String result = isLeap ? "leap" : "not leap";
+        System.out.println(result);
     }
+
 }
